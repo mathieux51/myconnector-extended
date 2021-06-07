dee1ccb687798c246363742e426398ce0476e235
Mathieu <mathieux51@gmail.com>
2021-06-07 16:31:00 +0200

    Add logger JSONToRecordTransforms

--- src/main/java/org/apache/camel/kafkaconnector/extended/JSONToRecordTransforms.java
+++ src/main/java/org/apache/camel/kafkaconnector/extended/JSONToRecordTransforms.java
@@ -7,11 +7,14 @@ import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.transforms.Transformation;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JSONToRecordTransforms<R extends ConnectRecord<R>> implements Transformation<R> {
     public static final String FIELD_KEY_CONFIG = "key";
     public static final ConfigDef CONFIG_DEF = new ConfigDef().define(FIELD_KEY_CONFIG, ConfigDef.Type.STRING, null, ConfigDef.Importance.MEDIUM,
                                                                       "Add the key and the header to the record value");
+    private static final Logger LOG = LoggerFactory.getLogger(JSONToRecordTransforms.class);
 
     @Override
     public void configure(Map<String, ?> configs) {
@@ -20,7 +23,9 @@ public class JSONToRecordTransforms<R extends ConnectRecord<R>> implements Trans
     @Override
     public R apply(R record) {
       Gson gson = new Gson();
-      return gson.fromJson((String) record.value(), new TypeToken<ConnectRecord<R>>(){}.getType());
+      LOG.warn("value.getClass().getSimpleName() {}",record.value().getClass().getSimpleName());
+      String str = new String((String) record.value());
+      return gson.fromJson(str, new TypeToken<ConnectRecord<R>>(){}.getType());
     }
 
     @Override
