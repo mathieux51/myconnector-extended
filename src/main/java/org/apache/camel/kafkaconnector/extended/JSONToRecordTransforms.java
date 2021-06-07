2d61a78f597759eea6e2ad58cc6a1ee00bc645ae
Mathieu <mathieux51@gmail.com>
2021-06-07 15:58:38 +0200

    Update JSONToRecordTransforms

---
+++ src/main/java/org/apache/camel/kafkaconnector/extended/JSONToRecordTransforms.java
@@ -0,0 +1,35 @@
+package org.apache.camel.kafkaconnector.extended;
+
+import java.util.Map;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import org.apache.kafka.connect.transforms.Transformation;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class JSONToRecordTransforms<R extends ConnectRecord<R>> implements Transformation<R> {
+    public static final String FIELD_KEY_CONFIG = "key";
+    public static final ConfigDef CONFIG_DEF = new ConfigDef().define(FIELD_KEY_CONFIG, ConfigDef.Type.STRING, null, ConfigDef.Importance.MEDIUM,
+                                                                      "Add the key and the header to the record value");
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+    }
+
+    @Override
+    public R apply(R record) {
+      Gson gson = new Gson();
+      return gson.fromJson((String) record.value(), new TypeToken<ConnectRecord<R>>(){}.getType());
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public ConfigDef config() {
+        return CONFIG_DEF;
+    }
+}
+
--- src/main/java/org/apache/camel/kafkaconnector/extended/RecordToJSONTransforms.java
+++ src/main/java/org/apache/camel/kafkaconnector/extended/RecordToJSONTransforms.java
@@ -1,36 +1,45 @@
 package org.apache.camel.kafkaconnector.extended;
 
+import com.google.gson.Gson;
 import java.util.Map;
-
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.transforms.Transformation;
-import com.google.gson.Gson;
 
 public class RecordToJSONTransforms<R extends ConnectRecord<R>> implements Transformation<R> {
-    public static final String FIELD_KEY_CONFIG = "key";
-    public static final ConfigDef CONFIG_DEF = new ConfigDef().define(FIELD_KEY_CONFIG, ConfigDef.Type.STRING, null, ConfigDef.Importance.MEDIUM,
-                                                                      "Add the key and the header to the record value");
+  public static final String FIELD_KEY_CONFIG = "key";
+  public static final ConfigDef CONFIG_DEF =
+      new ConfigDef()
+          .define(
+              FIELD_KEY_CONFIG,
+              ConfigDef.Type.STRING,
+              null,
+              ConfigDef.Importance.MEDIUM,
+              "Add the key and the header to the record value");
 
-    @Override
-    public void configure(Map<String, ?> configs) {
-    }
+  @Override
+  public void configure(Map<String, ?> configs) {}
 
-    @Override
-    public R apply(R record) {
-      Gson gson = new Gson();
-      String json = gson.toJson(record);
-      return record.newRecord(record.topic(), record.kafkaPartition(), null, record.key(), Schema.STRING_SCHEMA, json, record.timestamp());
-    }
+  @Override
+  public R apply(R record) {
+    Gson gson = new Gson();
+    String json = gson.toJson(record);
+    return record.newRecord(
+        record.topic(),
+        record.kafkaPartition(),
+        null,
+        record.key(),
+        Schema.STRING_SCHEMA,
+        json,
+        record.timestamp());
+  }
 
-    @Override
-    public void close() {
-    }
+  @Override
+  public void close() {}
 
-    @Override
-    public ConfigDef config() {
-        return CONFIG_DEF;
-    }
+  @Override
+  public ConfigDef config() {
+    return CONFIG_DEF;
+  }
 }
-
