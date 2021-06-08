1351dd8b1eceeea3222c4f9968d0818527a63356
Mathieu <mathieux51@gmail.com>
2021-06-08 11:53:21 +0200

    Revert "Remove adapter"
    This reverts commit e1226e8da4a048ce78f689b8485541ff12d63d32.

---
+++ src/main/java/org/apache/camel/kafkaconnector/extended/ConnectRecordAdapter.java
@@ -0,0 +1,30 @@
+package org.apache.camel.kafkaconnector.extended;
+
+import com.google.gson.*;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import java.lang.reflect.Type;
+
+public class ConnectRecordAdapter<R extends ConnectRecord<R>> implements JsonSerializer<R>, JsonDeserializer<R> {
+    @Override
+    public JsonElement serialize(R src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject result = new JsonObject();
+        result.add("className", new JsonPrimitive(src.getClass().getSimpleName()));
+        result.add("data", context.serialize(src, src.getClass()));
+
+        return result;
+    }
+
+    @Override
+    public R deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+        throws JsonParseException {
+        JsonObject jsonObject = json.getAsJsonObject();
+        String type = jsonObject.get("className").getAsString();
+        JsonElement element = jsonObject.get("data");
+
+        try {
+            return context.deserialize(element, Class.forName("org.apache.camel.kafkaconnector.extended." + type));
+        } catch (ClassNotFoundException cnfe) {
+            throw new JsonParseException("Unknown element type: " + type, cnfe);
+        }
+    }
+}
