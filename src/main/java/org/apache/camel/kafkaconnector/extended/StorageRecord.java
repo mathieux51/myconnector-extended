86a3e5cf39a9a52f1cfc3f01d54f22cf6c563cda
Mathieu <mathieux51@gmail.com>
2021-06-08 11:53:55 +0200

    Comment StorageRecord

--- src/main/java/org/apache/camel/kafkaconnector/extended/StorageRecord.java
+++ src/main/java/org/apache/camel/kafkaconnector/extended/StorageRecord.java
@@ -1,20 +1,20 @@
-package org.apache.camel.kafkaconnector.extended;
-
-import org.apache.kafka.connect.header.ConnectHeaders;
-import org.apache.kafka.connect.header.Headers;
-
-public class StorageRecord {
-  public final String key;
-  public final String body;
-  public final Headers headers;
-
-  public StorageRecord(String key, String body, Headers headers) {
-    this.key = key;
-    this.body = body;
-    if (headers instanceof ConnectHeaders) {
-      this.headers = (ConnectHeaders) headers;
-    } else {
-      this.headers = new ConnectHeaders(headers);
-    }
-  }
-}
+// package org.apache.camel.kafkaconnector.extended;
+//
+// import org.apache.kafka.connect.header.ConnectHeaders;
+// import org.apache.kafka.connect.header.Headers;
+//
+// public class StorageRecord {
+//   public final String key;
+//   public final String body;
+//   public final Headers headers;
+//
+//   public StorageRecord(String key, String body, Headers headers) {
+//     this.key = key;
+//     this.body = body;
+//     if (headers instanceof ConnectHeaders) {
+//       this.headers = (ConnectHeaders) headers;
+//     } else {
+//       this.headers = new ConnectHeaders(headers);
+//     }
+//   }
+// }
