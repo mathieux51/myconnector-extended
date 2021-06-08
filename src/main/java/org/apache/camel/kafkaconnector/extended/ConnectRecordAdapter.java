54241e99b14b3028bef259616098ca0730979ef7
Mathieu <mathieux51@gmail.com>
2021-06-08 09:48:06 +0200

    WIP adapter

--- src/main/java/org/apache/camel/kafkaconnector/extended/ConnectRecordAdapter.java
+++ src/main/java/org/apache/camel/kafkaconnector/extended/ConnectRecordAdapter.java
@@ -1,28 +1,30 @@
 package org.apache.camel.kafkaconnector.extended;
 
+import com.google.gson.*;
+import org.apache.kafka.connect.connector.ConnectRecord;
+import java.lang.reflect.Type;
 
-public class ConnectRecordAdapter  implements JsonSerializer<A>, JsonDeserializer<A> {
-  @Override
-  public JsonElement serialize(A src, Type typeOfSrc, JsonSerializationContext context) {
-      JsonObject result = new JsonObject();
-      result.add("type", new JsonPrimitive(src.getClass().getSimpleName()));
-      result.add("properties", context.serialize(src, src.getClass()));
-      return result;
-  }
+public class ConnectRecordAdapter<R extends ConnectRecord<R>> implements JsonSerializer<R>, JsonDeserializer<R> {
+    @Override
+    public JsonElement serialize(R src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject result = new JsonObject();
+        result.add("className", new JsonPrimitive(src.getClass().getSimpleName()));
+        result.add("data", context.serialize(src, src.getClass()));
 
-
-  @Override
-  public A deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-        throws JsonParseException {
-    JsonObject jsonObject = json.getAsJsonObject();
-    String type = jsonObject.get("type").getAsString();
-    JsonElement element = jsonObject.get("properties");
-
-    try {
-        String thepackage = "my.package.name.";
-        return context.deserialize(element, Class.forName(thepackage + type));
-    } catch (ClassNotFoundException cnfe) {
-        throw new JsonParseException("Unknown element type: " + type, cnfe);
+        return result;
     }
-  }
+
+    @Override
+    public R deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+        throws JsonParseException {
+        JsonObject jsonObject = json.getAsJsonObject();
+        String type = jsonObject.get("className").getAsString();
+        JsonElement element = jsonObject.get("data");
+
+        try {
+            return context.deserialize(element, Class.forName("org.apache.camel.kafkaconnector.extended." + type));
+        } catch (ClassNotFoundException cnfe) {
+            throw new JsonParseException("Unknown element type: " + type, cnfe);
+        }
+    }
 }
